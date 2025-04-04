ec5e74828ec7c785a5433dab4d24ea9abb3ec6d4
asawinSit <144443633+asawinSit@users.noreply.github.com>
2025-04-04 12:52:04 +0200

    shock effect

---
+++ Source/TheLyingCake/BlackOut_EffectComponent.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BlackOut_EffectComponent.h"
+#include "PlayerCharacter.h"
+#include "GameFramework/PlayerController.h"
+#include "Camera/CameraComponent.h"
+
+
+void UBlackOut_EffectComponent::ApplyEffect(AActor* Actor)
+{
+	Super::ApplyEffect(Actor);
+	if (Actor != nullptr)
+	{
+		EffectDuration = 1;
+		APlayerCharacter* PlayerCharacter = Cast<APlayerCharacter>(Actor);
+		if (PlayerCharacter != nullptr)
+		{
+			
+			UCameraComponent* PlayerCamera = PlayerCharacter->FindComponentByClass<UCameraComponent>();
+			if (PlayerCamera)
+			{
+				PlayerCamera->PostProcessSettings.bOverride_ColorGamma = true;
+				PlayerCamera->PostProcessSettings.ColorGamma = FVector4(0,0,0,0);
+			}
+			FTimerDelegate TimerDelegate = FTimerDelegate::CreateLambda([this, PlayerCamera]()
+			{
+				PlayerCamera->PostProcessSettings.bOverride_ColorGamma = false;
+				
+				this->DestroyComponent();
+			});
+			
+			GetOwner()->GetWorld()->GetTimerManager().SetTimer(EffectTimer, TimerDelegate, EffectDuration, false);
+		}
+	}
+}
